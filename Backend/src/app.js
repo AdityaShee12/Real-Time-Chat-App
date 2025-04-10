4c3de2a3fb41c449873c8c22c232e4ba41d38ab0
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-04-10 08:54:32 +0530

    Correct the origin of cors in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ import "./OAuth20.js";
 const app = express();
 
 // Middleware Setup
-app.use(cors({ origin: "https://real-time-chat-app-eight-delta.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://real-time-chat-app-eight-delta.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
