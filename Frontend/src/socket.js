39943d09cd1d9116ee85d4c307e1037843808c3a
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-04-10 12:22:16 +0530

    update socket.js in frontend

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://real-time-chat-app-3-axa5.onrender.com", {
   withCredentials: true,
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
 });
 
 export default socket;
